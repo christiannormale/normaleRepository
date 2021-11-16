d8ba0a254774685725851e377d1afa20ec9a172e
christiannormale <christian.normale.s17@liceoviconapoli.it>
2021-11-16 11:40:54 +0100

    Create es4.py

---
+++ es4.py
@@ -0,0 +1,10 @@
+a=int(input("inserici numero(a):"))
+b=int(input("inserisci numero(b):"))
+c=int(input("inserisci numero(c):"))
+
+if (a>b>c):
+    print("è maggiore:",a)
+elif(c>b):
+    print("è maggiore:",c)
+else:
+    print("è maggiore:",b)
